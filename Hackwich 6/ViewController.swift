d26f42ef07f717d158efa780251a3fad1b0d6a41
Cassandra K <ckk30@hawaii.edu>
2023-10-17 12:27:59 -1000

    completed steps 1-8

--- Hackwich 6/ViewController.swift
+++ Hackwich 6/ViewController.swift
@@ -7,8 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var myFriendsArray = ["Daphnie", "Makamae", "Orion"]
+   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>; func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 1;
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+               let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+                       let text = myFriendsArray[indexPath.row]
+                       cell.textLabel?.text = text
+                       return cell
+          
 
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
